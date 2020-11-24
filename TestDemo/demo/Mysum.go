fcc5cffe75e92d48fa1e977461d1120ce39c36f6
sdccone1 <171942067@qq.com>
2020-11-24 22:03:37 +0800

    "the fourth commit" "update Mysum.go and mysum_test.go" "新增了对数组和切片slices进行求和的API，以及相应的测试函数"

--- TestDemo/demo/Mysum.go
+++ TestDemo/demo/Mysum.go
@@ -1,9 +1,23 @@
 package demo
 
-func Mysum(data []int) int { //此时Mysum()的参数必须要是int 类型的数组 且不能指定具体的长度 []int
+func MysumForArray(data [5]int) (sum int) { //MysumForArray()的参数必须是长度为5的int型数组
 	var totalSum int = 0
 	for i := 0; i < len(data); i++ {
 		totalSum += data[i]
+	}
+	return totalSum
+}
+func MysumForSlices(data []int) (sum int) { //此时MysumForSlices的参数就是一个slices切片了
+	var totalSum int = 0
+	//range 关键字可以修饰一个集合(可迭代的对象)其返回两个值：(index , value),且可以 利用range 关键字来遍历修改一个集合
+
+	//先令原切片data的每个元素自增1
+	for idx, _ := range data {
+		data[idx]++
+	}
+	//再求和
+	for _, num := range data { // 使用空白标识符(下划线)来忽略原始值
+		totalSum += num
 	}
 	return totalSum
 }
--- TestDemo/mysum_test.go
+++ TestDemo/mysum_test.go
@@ -2,29 +2,42 @@ package main
 
 /**
 @author：DavidMa
-@Content:为demo.Mysum()编写测试用例，注意这里的数组定义方式
+@Content:为demo.Mysum()编写测试用例，注意这里的数组和切片的定义方式以及区别，特别是切片，很类似Java中的ArrayList
 @Date : 2020-11-24 21:09
 */
 import "testing"
 import "./demo"
 
 func TestMysum(t *testing.T) {
+
 	assertCorrectMessage := func(t *testing.T, got, want int) {
 		t.Helper()
 		if got != want {
 			t.Errorf("got: '%d' want: '%d'", got, want)
 		}
 	}
-	t.Run("test1 for demo.Mysum()", func(t *testing.T) {
-		data1 := []int{1, 2, 3, 4, 5}
-		got := demo.Mysum(data1)
+	t.Run("test1 for demo.MysumForArray()", func(t *testing.T) {
+		data := [5]int{1, 2, 3, 4, 5}
+		got := demo.MysumForArray(data)
 		want := 15
 		assertCorrectMessage(t, got, want)
 	})
-	t.Run("test2 for demo.Mysum()", func(t *testing.T) {
-		data2 := []int{1, 2, 3, 4, 5, 6}
-		got := demo.Mysum(data2)
-		want := 15
+	//下面展示了定义slices切片的两种方式
+	t.Run("test1 for demo.MysumForSlices()", func(t *testing.T) {
+		dataSlices := []int{1, 2, 3, 4, 5}
+		got := demo.MysumForSlices(dataSlices)
+		want := 20
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("test2 for demo.MysumForSlices()", func(t *testing.T) {
+		dataSlices := make([]int, 5)
+		i := 1
+		for idx, _ := range dataSlices {
+			dataSlices[idx] = i
+			i++
+		}
+		got := demo.MysumForSlices(dataSlices)
+		want := 20
 		assertCorrectMessage(t, got, want)
 	})
 }
