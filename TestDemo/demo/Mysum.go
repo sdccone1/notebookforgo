fb61f3f66e8cd30b69706aa9239c2609bd626747
sdccone1 <171942067@qq.com>
2020-11-24 21:15:55 +0800

    "the third commit" "update Mysum.go "

--- TestDemo/demo/Mysum.go
+++ TestDemo/demo/Mysum.go
@@ -1,6 +1,6 @@
 package demo
 
-func Mysum(data []int) int {
+func Mysum(data []int) int { //此时Mysum()的参数必须要是int 类型的数组 且不能指定具体的长度 []int
 	var totalSum int = 0
 	for i := 0; i < len(data); i++ {
 		totalSum += data[i]
